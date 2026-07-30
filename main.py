81072b1e6363081edba485fdcfa446c42a0db0ea
Dereckrk1 <dereckmera84@gmail.com>
2026-07-30 16:53:20 -0500

    Refactor main function to use 'ferre' variable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from registro import VentanaRegistro
-from opciones import VentanaOpciones
- 
+from opciones import VentanaOpciones 
 def main():
- app = VentanaRegistro(imagen_path="santa_lucia.jpg")
- app.ventana_opciones = VentanaOpciones(app)
- app.mainloop() 
+ ferre = VentanaRegistro(imagen_path="santa_lucia.jpg")
+ ferre.ventana_opciones = VentanaOpciones(ferre)
+ ferre.ventana_opciones.withdraw()
+ ferre.mainloop() 
  
 if __name__ == "__main__":
  main()
